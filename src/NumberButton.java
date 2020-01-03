4e81e15d2a2801195fb2c98d12a95a2fb1498fa2
ntucschen <r06548051@ntu.edu.tw>
2020-01-03 18:11:33 +0800

    有bug;將listener拉出來至NumberButton,可以work,但main無法監聽到修改隨著改displayA

--- src/NumberButton.java
+++ src/NumberButton.java
@@ -1,8 +1,47 @@
 import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class NumberButton extends JButton{
-	public NumberButton(String label, int x, int y, int w, int h) {
-        super(label);
-        super.setBounds(x, y, w, h);
-    }
+public abstract class NumberButton extends JButton{
+	
+	protected String displayA;
+	protected NumberButton(String label) {
+		super(label);
+		super.addActionListener(new ActionListener() { //增加exception來提醒是哪裡錯 少什麼？
+		      public void actionPerformed(ActionEvent e) {
+		        displayA = label;
+		        System.out.printf(displayA);
+//		        numDisplay.setText(displayA);
+		      }
+		    });
+		displayA = "" ;
+	}
+	
+	protected void setBtnPosition(int x, int y ,int w, int h) {
+		super.setBounds(x, y, w, h);
+	}
+	protected String getDisplayA() {
+		return displayA;
+	}
+	
+//	protected void setDisplayA(String displayA) {
+//		displayA = displayA;
+//	}
+
+
+	/*
+	     zero.addActionListener(new ActionListener() { //增加exception來提醒是哪裡錯 少什麼？
+      public void actionPerformed(ActionEvent e) {
+        displayA = displayA + "0";
+        numDisplay.setText(displayA);
+      }
+    });
+
+	 */
+	
+//	public NumberButton(String label, int x, int y, int w, int h) {
+//        super(label);
+//        super.setBounds(x, y, w, h);
+//    }
+	
 }
